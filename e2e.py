81901a9ab769f03d25c9cafcd7a555ce4b0a5a12
Icemanm <95038099+Icemanm@users.noreply.github.com>
2023-05-18 22:18:21 +0300

    Update e2e.py port

--- e2e.py
+++ e2e.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import sys
 
 # it’s purpose is to test our web service. It will get the application
 # URL as an input, open a browser to that URL, select the score element in our web page,
 # check that it is a number between 1 to 1000 and return a boolean value if it’s true or not.
 def test_scores_service(url):
-    my_driver = webdriver.Chrome('C:\\Users\\moshi\\Downloads\\chromeDriver\\chromedriver.exe')
+    service = Service('C:\\Users\\moshi\\Downloads\\chromeDriver\\chromedriver.exe')
+    my_driver = webdriver.Chrome(service=service)
     my_driver.get(url)
     score = my_driver.find_element(By.ID, 'score')
     s = int(score.text)
@@ -16,16 +18,12 @@ def test_scores_service(url):
     else:
         return False
 
-    # print(score.text)
-    # print(type(score.text))
-
-
 
 # call our tests function. The main function will return -1 as an OS exit
 # code if the tests failed and 0 if they passed.
 
 
-def main_function(url='http://127.0.0.1:5000/score'):
+def main_function(url='http://127.0.0.1:8777/'):
     result = test_scores_service(url)
     if result == True:
         sys.exit(0)
@@ -34,6 +32,6 @@ def main_function(url='http://127.0.0.1:5000/score'):
     else:
         print("unknown result")
 
-# url = 'http://127.0.0.1:5000/score'
-main_function()
+        
+ main_function()
 
